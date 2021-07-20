a7466fb428ef873aa2c884d1caa669069db8cb29
MateDawid <mateusiakdawid@gmail.com>
2021-07-20 18:31:43 +0200

    Add Keyword model to models and admin display

--- blogsite/blog/admin.py
+++ blogsite/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Keyword
 
 from django_summernote.admin import SummernoteModelAdmin
 
@@ -12,4 +12,5 @@ class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
 
 
+admin.site.register(Keyword)
 admin.site.register(Post, PostAdmin)
