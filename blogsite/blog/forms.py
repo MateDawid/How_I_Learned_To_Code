076e3e0bdcf9cc31a5f92444de508ac67d0dd1bc
MateDawid <mateusiakdawid@gmail.com>
2021-07-26 15:56:27 +0200

    Make content in forms not required - keyword is enough

--- blogsite/blog/forms.py
+++ blogsite/blog/forms.py
@@ -3,7 +3,7 @@ from .models import Post, Keyword
 
 
 class SearchForm(forms.Form):
-    content = forms.CharField(label="Czego szukasz?", help_text="", widget=forms.TextInput(attrs={'class': 'form_field'}))
+    content = forms.CharField(label="Czego szukasz?", help_text="", required=False, widget=forms.TextInput(attrs={'class': 'form_field'}))
     keyword = forms.ChoiceField(label="W jakim temacie?", help_text="", widget=forms.Select(attrs={'class': 'form_field'}))
 
     def __init__(self, *args, **kwargs):
